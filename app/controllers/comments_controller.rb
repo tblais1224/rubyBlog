cc9fd35340ca50152326f23bfecfeb7aa77d2402
tblais1224 <thomasblais.student@careerdevs.com>
2019-11-12 15:26:53 -0500

    added simple auth system

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,6 @@
 class CommentsController < ApplicationController
+    http_basic_authenticate_with name: "Tom", password: "testAuth", only: [:destroy]
+
     def create
         @post = Post.find(params[:post_id])
         @comment = @post.comments.create(comment_params)
